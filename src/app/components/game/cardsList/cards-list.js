5fd639372b554a163e8b71555bf805de1c656fe5
losJasones <jasoneslos@gmail.com>
2017-05-15 18:36:01 +0200

    Corrección de bug
    Hasta ahora, si el usuario era muy rápido, se podían destapar más de 2 cartas antes de que se volvieran a ocultar. Si las 2 primeras casaban, no sólo desaparecían estas 2 sino también la tercera destapada, de forma que el juego quedaba en un estado inconsistente. Ya está arreglado, ahora no se permite destapar más de 2 cartas antes de comprobar si casan o no.

--- src/app/components/game/cardsList/cards-list.js
+++ src/app/components/game/cardsList/cards-list.js
@@ -6,52 +6,53 @@
     controllerAs: 'cardsList'
   });
 
-  function cardsList($state, $timeout, couplesFactory) {
+function cardsList($state, $timeout, couplesFactory) {
     var vm = this;
     vm.$onInit = function () {
       vm.remainingCards = couplesFactory.shuffleCards();
       vm.guesses = 0;
       vm.misses = 0;
       vm.shownCardsNumber = 0;
-      vm.firstShownCardId=-1;
-      vm.showSuccessMessage=false;
+      vm.firstShownCardId = -1;
+      vm.showSuccessMessage = false;
     };
 
     vm.actions = {};
 
     vm.actions.showCard = function (id) {
-      if(vm.firstShownCardId != id){
+      if (vm.firstShownCardId != id && vm.shownCardsNumber < 2) {
         vm.shownCardsNumber++;
         couplesFactory.showCardById(id);
-        vm.firstShownCardId=id;
-      }
-      if (vm.shownCardsNumber > 1) {
-        vm.actions.updateGuessesOrMisses();
-        vm.firstShownCardId=-1;
+        vm.firstShownCardId = id;
+        if (vm.shownCardsNumber == 2) {
+          vm.actions.updateGuessesOrMisses();
+        }
       }
     };
 
     vm.actions.updateGuessesOrMisses = function () {
-      vm.shownCardsNumber = 0;
       $timeout(function () {
         if (couplesFactory.isGuessCorrect()) {
           vm.guesses++;
           couplesFactory.deleteVisibleCards();
-          if(vm.guesses>7){
-            vm.showSuccessMessage=true;
+          if (vm.guesses > 7) {
+            vm.showSuccessMessage = true;
           }
         } else {
           vm.misses++;
           couplesFactory.hideCards();
         }
-      }, 500);
+        vm.shownCardsNumber = 0;
+        vm.firstShownCardId = -1;
+      }, 750);
     };
-    
+
     vm.actions.startAgain = function () {
-      vm.remainingCards=couplesFactory.shuffleCards();
-      vm.guesses=0;
-      vm.misses=0;
-      vm.showSuccessMessage=false;
+      vm.remainingCards = couplesFactory.shuffleCards();
+      vm.shownCardsNumber = 0;
+      vm.guesses = 0;
+      vm.misses = 0;
+      vm.showSuccessMessage = false;
     };
   }
 })(angular);

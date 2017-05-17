a0924dc6c2dbfdc56fcb6726f3315d8e6b61ccff
losJasones <jasoneslos@gmail.com>
2017-05-17 19:43:04 +0200

    Actualización de las nuevas tablas y servicios de back
    Sólo falta el servicio de los scores

--- couples-back/src/main/java/couples/controlers/ScoreController.java
+++ couples-back/src/main/java/couples/controlers/ScoreController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import couples.model.Score;
+import couples.model.User;
+import couples.model.UserScore;
 import couples.repositories.ScoreRepository;
+import couples.repositories.UserRepository;
 
 
 
@@ -23,34 +26,44 @@ import couples.repositories.ScoreRepository;
 public class ScoreController {
 
     @Autowired
-    ScoreRepository repository;   
+    private ScoreRepository scoreRepository;   
+    @Autowired
+    private UserRepository userRepository;   
     
 
     @RequestMapping(method = RequestMethod.POST)
     public Object create(@RequestBody Score score) {
 
-        score = repository.save(score);
+        score = scoreRepository.save(score);
 
         return Collections.singletonMap("id", score.getId());
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    /*public List<Score> findByTen(){
+   /* @RequestMapping(method = RequestMethod.GET,value="/topten")
+    public List<Score> findByTen(){
     	return findByTen();
     }*/
     
-    public List<Score> list() {
-        List<Score> score = new ArrayList<>();
-
-        Iterable<Score> it = repository.findAll();
+    @RequestMapping(method = RequestMethod.GET)
+    public List<UserScore> list() {
+        List<UserScore> userScores = new ArrayList<UserScore>();
         
+        
+        Iterable<Score> it = scoreRepository.findAll();
         Iterator<Score> iterator = it.iterator();
-        
         while(iterator.hasNext()) {
-            score.add(iterator.next());
+        	Score score = (Score) iterator.next();
+        	User user = userRepository.findByEmail(score.getEmail());        	
+            UserScore userScore = new UserScore();
+            userScore.setEmail(user.getEmail());
+            userScore.setName(user.getName());
+            userScore.setLastname(user.getLastname());
+            userScore.setNumfails(score.getNumfails());
+            userScores.add(userScore);
+            System.out.println(userScore.getEmail());
         }
 
-        return score;
+        return userScores;
     }
 }
 
--- couples-back/src/main/java/couples/repositories/ScoreRepository.java
+++ couples-back/src/main/java/couples/repositories/ScoreRepository.java
@@ -3,15 +3,15 @@ package couples.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.*;
 
 import couples.model.Score;
 
 public interface ScoreRepository extends CrudRepository<Score, Integer>  {
 	
 	
-	//@Query("SELECT s.numfails,u.NAME,u.EMAIL,u.AVATAR FROM SCORES s, USERS u WHERE s.USERS_ID=u.ID ORDER BY s.numfails ASC ")
+	//@Query("SELECT s , u FROM Score s, User u WHERE s.email=u.email")
+	//@Query (" SELECT s, u FROM Score s, User u INNER JOIN u.email s")
 	//List<Score> findByTen();
-
-	
+    
 }

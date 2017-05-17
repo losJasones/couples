ab712721c5c58cff149ec4e0749c16471eaaf26b
losJasones <jasoneslos@gmail.com>
2017-05-17 11:18:21 +0200

    gulpfile Aida
    Comento eslint y añado browserSync default y el reload on change en varias extensiones.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
+//const eslint = require('gulp-eslint');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
@@ -13,14 +13,19 @@ gulp.task('browserSync', function() {
   });
 
   gulp.watch("src/js/**/*.js").on("change", reload);
+  gulp.watch("src/**/*.html").on("change", reload);
+  gulp.watch("src/app/styles/*.css").on("change", reload);
+  gulp.watch("src/app/styles/**/*.jpg").on("change", reload);
+  gulp.watch("src/app/styles/**/*.png").on("change", reload);
 });
 
-gulp.task('lint', () => {
-  return gulp.src(['src/**/*.js'])
+/*gulp.task('lint', () => {
+  return gulp.src(['src/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});*/
+
+gulp.task('default', ['browserSync'], function() {
+    console.log('Default');
 });
-
-
-

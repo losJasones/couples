e131d232aaad0c7357c3cb31f38415ebea57b4a5
Alberto Carretero Arribas <Alberto Carretero Arribas>
2017-05-28 14:08:31 +0200

    ACtualización previa a presentación del proyecto.
    Modificación de las pantallas topscores, login y gameHeader para la
    presentación del proyecto.

--- src/app/components/login/view-login.js
+++ src/app/components/login/view-login.js
@@ -1,29 +1,30 @@
 (function (angular) {
 
-  angular.module('app').component('viewLogin', {
+ angular.module('app').component('viewLogin', {
     templateUrl: 'app/components/login/view-login-template.html',
     controller: ['$state', 'couplesFactory', viewLogin],
     controllerAs: 'viewLogin'
   });
 
-  function viewLogin($state, couplesFactory) {
+ function viewLogin($state, couplesFactory) {
     var vm = this;
 
-    vm.$onInit = function () {
+   vm.$onInit = function () {
       vm.showErrorMsg = false;
       vm.showOldErrorMsg = false;
+      vm.showNotNewErrorMsg = false;
       vm.showNotOldErrorMsg = false;
       vm.newFormIsSend = false;
       vm.oldFormIsSend = false;
     };
 
-    vm.actions = {};
+   vm.actions = {};
 
-    vm.actions.getAvatar = function (id) {
+   vm.actions.getAvatar = function (id) {
       return couplesFactory.getAvatars()[id - 1].img;
     }
 
-    vm.actions.goToGameFromNew = function () {
+   vm.actions.goToGameFromNew = function () {
       vm.showOldErrorMsg = false;
       vm.showNotOldErrorMsg = false;
       vm.oldFormIsSend = false;
@@ -34,48 +35,59 @@
       } else {
         vm.showErrorMsg = false;
         vm.newFormIsSend = false;
-        var newPlayer = {
-          firstName: vm.firstName,
-          lastName: vm.lastName,
-          email: vm.newEmail,
-          avatar: {
-            id: vm.avatar.substring(6),
-            name: vm.avatar,
-            img: vm.avatar + '.png'
-          }
+        var newPlayer;
+        var promise = couplesFactory.getUser(vm.newEmail);
+        promise.then(function (restResponse) {
+          newPlayer = restResponse;
+          if (newPlayer.email) {
+            vm.showNotNewErrorMsg = true;
+            vm.newFormIsSend = true;
+          } else {
+            newPlayer = {
+              firstName: vm.firstName,
+              lastName: vm.lastName,
+              email: vm.newEmail,
+              avatar: {
+                id: vm.avatar.substring(6),
+                name: vm.avatar,
+                img: vm.avatar + '.png'
+              }
+            }
+            couplesFactory.saveUser(vm.firstName, vm.lastName, vm.newEmail, vm.avatar);
+            couplesFactory.shuffleCards();
+            $state.go('game', { player: newPlayer });
+          };
+         }, function (error) { console.log(error) });
         }
-        couplesFactory.saveUser(vm.firstName, vm.lastName, vm.newEmail, vm.avatar);
-        couplesFactory.shuffleCards();
-        $state.go('game', { player: newPlayer });
       }
-    }
 
-    vm.actions.goToGameFromOld = function () {
-      vm.showErrorMsg = false;
-      vm.newFormIsSend = false;
-      vm.newEmail = '';
-      vm.firstName = '';
-      vm.lastName = '';
+ vm.actions.goToGameFromOld = function () {
+    vm.showErrorMsg = false;
+    vm.newFormIsSend = false;
+    vm.showNotNewErrorMsg = false;
+    vm.newEmail = '';
+    vm.firstName = '';
+    vm.lastName = '';
 
-      if (!vm.oldEmail) {
-        vm.showOldErrorMsg = true;
-        vm.oldFormIsSend = true;
-      } else {
-        vm.showOldErrorMsg = false;
-        vm.oldFormIsSend = false;
-        var oldPlayer={};
-        var promise = couplesFactory.getUser(vm.oldEmail);
-        promise.then(function(restResponse){
-          oldPlayer=restResponse;
-          if (!oldPlayer || !oldPlayer.email) {
+   if (!vm.oldEmail) {
+      vm.showOldErrorMsg = true;
+      vm.oldFormIsSend = true;
+    } else {
+      vm.showOldErrorMsg = false;
+      vm.oldFormIsSend = false;
+      var oldPlayer = {};
+      var promise = couplesFactory.getUser(vm.oldEmail);
+      promise.then(function (restResponse) {
+        oldPlayer = restResponse;
+        if (!oldPlayer || !oldPlayer.email) {
           vm.showNotOldErrorMsg = true;
           vm.oldFormIsSend = true;
         } else {
           couplesFactory.shuffleCards();
           $state.go('game', { player: oldPlayer });
         };
-        }, function(error){console.log(error)}); 
-      }
+      }, function (error) { console.log(error) });
     }
   }
+}
 })(angular);
